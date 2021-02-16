25298fc6dbb59bde8d2cf60e1101febc46dc742b
Nguyentronghoang <nguyentronghoang.bk@gmail.com>
2021-02-16 21:34:08 +0700

    customer manager app init

---
+++ src/components/Customers.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="customers">
+    <h1>Customer</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Customers",
+  data() {
+    return {};
+  },
+};
+</script>
+
+<style lang="scss" scoped></style>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,28 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import VueRouter from 'vue-router' 
+import vueResource from 'vue-resource'
+import Customers from './components/Customers.vue';
+import About from './components/About.vue';
 
-createApp(App).mount('#app')
+
+ const router = new VueRouter({
+     mode: "history",
+     base: __dirname,
+     routes: [
+         {path:'/', component: Customers},
+         {path:'/about', component: About}
+     ]
+ });
+ 
+ createApp(App, router).use(VueRouter).use(vueResource).mount('#app')
+
+//  {
+//     router,
+//     template: `
+//     <div id ="app">
+//     <router-link to="/about">About</router-link>
+//     <router-view></router-view>
+//     </div>
+//     `
+// }
