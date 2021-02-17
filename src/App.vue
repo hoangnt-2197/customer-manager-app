ccae932bbe89fc84fcea67a4f1cec862ef32c72f
Nguyen Trong Hoang (hoangnt-2197) <nguyen.trong.hoang@sun-asterisk.com>
2021-02-17 02:51:19 -0900

    add router and boostrap , jquery

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,47 @@
 <template>
-<div id="app">
-  <h1>App</h1>
-</div>
+  <div id="app">
+    <h1>App</h1>
+    <nav class="navbar navbar-default">
+      <div class="container">
+        <div class="navbar-header">
+          <button
+            type="button"
+            class="navbar-toggle collapsed"
+            data-toggle="collapse"
+            data-target="#navbar"
+            aria-expanded="false"
+            aria-controls="navbar"
+          >
+            <span class="sr-only">Toggle navigation</span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+            <span class="icon-bar"></span>
+          </button>
+          <a class="navbar-brand" href="#">vCustomer</a>
+        </div>
+        <div id="navbar" class="collapse navbar-collapse">
+          <ul class="nav navbar-nav">
+            <li class="active">
+              <router-link to="/">Customers</router-link>
+            </li>
+            <li>
+              <router-link to="/about">About</router-link>
+            </li>
+          </ul>
+        </div>
+        <!--/.nav-collapse -->
+      </div>
+    </nav>
+    <router-view />
+  </div>
+  
 </template>
 <script>
-
 export default {
-  name: 'App',
-  components: {
-  }
-}
+  name: "App",
+  components: {}
+};
 </script>
 
 <style>
-
 </style>
--- src/main.js
+++ src/main.js
@@ -1,28 +1,28 @@
 import { createApp } from 'vue'
 import App from './App.vue'
-import VueRouter from 'vue-router' 
-import vueResource from 'vue-resource'
-import Customers from './components/Customers.vue';
-import About from './components/About.vue';
+import { createWebHistory, createRouter } from "vue-router"
+import Customers from './components/Customers.vue'
+import About from './components/About.vue'
+
+const routes = [
+    {
+        path: "/",
+        name: "Customers",
+        component: Customers,
+    },
+    {
+        path: "/about",
+        name: "About",
+        component: About,
+    },
+];
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes,
+});
 
 
- const router = new VueRouter({
-     mode: "history",
-     base: __dirname,
-     routes: [
-         {path:'/', component: Customers},
-         {path:'/about', component: About}
-     ]
- });
- 
- createApp(App, router).use(VueRouter).use(vueResource).mount('#app')
 
-//  {
-//     router,
-//     template: `
-//     <div id ="app">
-//     <router-link to="/about">About</router-link>
-//     <router-view></router-view>
-//     </div>
-//     `
-// }
+
+createApp(App).use(router).mount('#app')
